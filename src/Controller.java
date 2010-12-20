f3c0417f00f76fcf4d932b17a68322a1b999f0fd
nutzer <nutzer@192.168.0.128>
2010-12-20 11:31:10 +0100

    20.12.2010

--- src/Controller.java
+++ src/Controller.java
@@ -8,23 +8,31 @@ public class Controller {
 		Paket paket1 = new Paket(5, 10, 10, 5);
 		Paket paket2 = new Paket(15, 10, 10, 5);
 		Paket paket3 = new Paket(10, 10, 10, 5);
+		Paket paket4 = new Paket(10, 5, 5, 5);
 		
 		paket1.positionieren(0, 0, 0);
 		paket2.positionieren(11, 0, 0);
 		paket3.positionieren(0, 11, 0);
+		paket4.positionieren(0, 11, 0);
 		
 		c.einladen(paket1);
 		c.einladen(paket2);
 		c.einladen(paket3);
+		c.einladen(paket4);
 		
 		ArrayList<Paket> pakete = new ArrayList<Paket>();
 		pakete.add(paket1);
 		pakete.add(paket2);
 		pakete.add(paket3);
+		pakete.add(paket4);
 		Collections.sort(pakete);
-		for (Paket p: pakete) {
-			System.out.println(p.getBreite());
-		}
+		//for (Paket p: pakete) {
+		//	System.out.println(p.getBreite());
+		//}
+		
+		Turm t = new Turm(25);
+		t.turmBauen(pakete);
+		t.anzeigen();
 	}
 	
 	public ArrayList<Paket> sortiere(ArrayList<Paket> pakete) {
--- src/Turm.java
+++ src/Turm.java
@@ -8,5 +8,29 @@ public class Turm {
 		maxHoehe = h;
 	}
 	
+	public int aktuelleHoehe() {
+		int h = 0;
+		for (Paket p: stapel) {
+			h += p.getHoehe();
+		}
+		return h;
+	}
 	
+	public int verbleibendeHoehe() {
+		return maxHoehe - aktuelleHoehe();
+	}
+	
+	public void turmBauen(ArrayList<Paket> pakete) {
+		for (Paket p: pakete) {
+			if (p.getHoehe() <= verbleibendeHoehe()) {
+				stapel.add(p);
+			}
+		}
+	}
+	
+	public void anzeigen() {
+		for (Paket p: stapel) {
+			System.out.println(p.getBreite());
+		}
+	}
 }
